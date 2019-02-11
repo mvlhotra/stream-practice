fe7468c8fa02d647eb0c5a53d2c5e1302ba71812
Nik Malhotra <nmal@me.com>
2019-02-11 17:31:53 +0000

    4th step done

---
+++ stream4.js
@@ -0,0 +1,27 @@
+function printHTML(html){
+    console.log(html);
+    return;
+}
+
+function getHTML(options,callback){
+    const https = require('https'); 
+    let myData = '';
+    https.get(options, function (stream) {
+       stream.setEncoding('utf8');
+       stream.on('data', function (chunk) {
+        myData += chunk + "\n";
+      }); 
+      stream.on('end', function(){
+        callback(myData);
+      });
+    });
+    return;
+}
+
+const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+  };
+
+getHTML(requestOptions,printHTML);
+
